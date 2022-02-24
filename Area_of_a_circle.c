6cb3f8a54b454d47fdc2410145cb1c25fe0c7611
21p31a04k2 <99868559+21p31a04k2@users.noreply.github.com>
2022-02-24 15:03:16 +0530

    #include<stdio.h> int main() {     float r,area;     float pie=3.14;     //printf("enter n values");     scanf("%f",&r);     area=3.14*r*r;     printf("%.2f",area);     return 0; }

---
+++ Area_of_a_circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float r,area;
+    float pie=3.14;
+    //printf("enter n values");
+    scanf("%f",&r);
+    area=3.14*r*r;
+    printf("%.2f",area);
+    return 0;
+}
